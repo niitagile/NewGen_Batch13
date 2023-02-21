83e838ef6c64a9f4745e28f6184452ff3ba7d641
niitagile <43024343+niitagile@users.noreply.github.com>
2023-02-21 17:55:08 +0530

    Update shema.sql

--- mssql/shema.sql
+++ mssql/shema.sql
@@ -1,12 +1,4 @@
-/*
---------------------------------------------------------------------
-© 2017 sqlservertutorial.net All Rights Reserved
---------------------------------------------------------------------
-Name   : BikeStores
-Link   : http://www.sqlservertutorial.net/load-sample-database/
-Version: 1.0
---------------------------------------------------------------------
-*/
+
 -- create schemas
 CREATE SCHEMA production;
 go
