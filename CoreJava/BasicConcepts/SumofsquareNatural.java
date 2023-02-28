39d9193f20ac64e9babdabdaeb3c0b6ce5a2c124
niitagile <43024343+niitagile@users.noreply.github.com>
2023-02-28 15:08:47 +0530

    Add files via upload

---
+++ CoreJava/BasicConcepts/SumofsquareNatural.java
@@ -0,0 +1,17 @@
+package ControlStatements;
+
+
+public class SumofsquareNatural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		System.out.println("start");
+		for(int i=1; i<=10; i++)
+		{
+			sum=sum+(i*i);
+		}
+		System.out.println(sum);
+	}
+
+}
