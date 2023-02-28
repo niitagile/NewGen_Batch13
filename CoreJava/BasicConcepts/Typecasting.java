3047a68fd5f4490a072c9a9710794df330abb19b
niitagile <43024343+niitagile@users.noreply.github.com>
2023-02-28 17:31:13 +0530

    Add files via upload

---
+++ CoreJava/BasicConcepts/Typecasting.java
@@ -0,0 +1,17 @@
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		int a=45;
+		//Explicit conversion
+		byte c=(byte)a;
+		System.out.println(c);
+		float b=89.6f;
+		//implict
+		float sum=a+b;
+		System.out.println(sum);
+	
+
+	}
+
+}
